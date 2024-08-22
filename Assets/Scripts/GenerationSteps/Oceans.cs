f5164f38d64b8935bf44ba28e999869c08bab14c
Vilgot Ottestig <vilgot.ottestig@gmail.com>
2024-08-22 14:42:27 +0200

    Added basic non-random ocean generation.

--- Assets/Scripts/GenerationSteps/Oceans.cs
+++ Assets/Scripts/GenerationSteps/Oceans.cs
@@ -1,11 +1,47 @@
 using UnityEngine;
 
 [CreateAssetMenu(menuName = "GenerationSteps/Oceans", fileName = "Oceans")]
-public class Oceans : GenerationStep{
+public class Oceans : GenerationStep {
+    private const int Left = -1, Right = +1;
+    private const float QuarterTurn = Mathf.PI/2;
+    
     [SerializeField] private int waterWidth, waterDepth;
     [SerializeField] private int seaBedThickness;
     [SerializeField] private int beachWidth, beachDepth;
     public override float Perform(BlockType[,] worldGrid, WorldSize worldSize, Seed seed){
+        CreateOcean(worldGrid, worldSize, seed, Left);
+        CreateOcean(worldGrid, worldSize, seed, Right);
         return 1;
+    }
+
+    private void CreateOcean(BlockType[,] worldGrid, WorldSize worldSize, Seed seed, int side){
+        int seaLevel = worldSize.seaLevel;
+        int edge = side == Left ? -1 : worldSize.width;
+        int x = edge - side*(waterWidth+beachWidth);
+        for (int i = 0; i < beachWidth; i++){
+            int y = worldSize.height-1;
+            for (; y > seaLevel; y--){
+                worldGrid[x, y] = BlockType.Air;
+            }
+            for (; y > seaLevel-beachDepth; y--){
+                worldGrid[x, y] = BlockType.Sand;
+            }
+            x += side;
+        }
+        for (int i = 0; i < waterWidth; i++){
+            int localWaterBottom = seaLevel-(int)(waterDepth * Mathf.Sin(i*QuarterTurn/waterWidth));
+            int localSeaBedBottom = Mathf.Min(localWaterBottom - seaBedThickness, seaLevel-beachDepth);
+            int y = worldSize.height-1;
+            for (; y > worldSize.seaLevel; y--){
+                worldGrid[x, y] = BlockType.Air;
+            }
+            for (; y > localWaterBottom; y--){
+                worldGrid[x, y] = BlockType.Water;
+            }
+            for (; y > localSeaBedBottom; y--){
+                worldGrid[x, y] = BlockType.Sand;
+            }
+            x += side;
+        }
     }
 }
