22ae6fb685ef1273a5ac91f8687c914ee6a8386d
FireLordBulb <79010333+FireLordBulb@users.noreply.github.com>
2024-08-23 13:45:58 +0200

    Made one block wide cave walk pseudo-randomly mostly downwards.

--- Assets/Scripts/GenerationSteps/Caves.cs
+++ Assets/Scripts/GenerationSteps/Caves.cs
@@ -1,9 +1,15 @@
+using System;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 [CreateAssetMenu(menuName = "GenerationSteps/Caves", fileName = "Caves")]
 public class Caves : GenerationStep {
     [SerializeField] private float lengthScalar;
-
+    [Range(0, 1)]
+    [SerializeField] private float keepDirectionWeight;
+    [Range(0, 1)]
+    [SerializeField] private float upwardsWeight;
+    
     private static readonly Vector2Int[] Directions = {new(-1, +1), Vector2Int.left, new(-1, -1), Vector2Int.down, new(+1, -1), Vector2Int.right, new(+1, +1)};
     
     public override float Perform(BlockType[,] worldGrid, int[] elevations, WorldSize worldSize, Seed seed){
@@ -12,6 +18,7 @@ public class Caves : GenerationStep {
         int x = Random.Range(0, elevations.Length);
         int y = elevations[x];
         Vector2Int direction = Vector2Int.down;
+        int directionIndex = Array.FindIndex(Directions, vector => vector == direction);
         for (int i = 0; i < walkMaxSteps; i++){
             worldGrid[x, y] = worldGrid[x, y] switch {
                 BlockType.Rock => BlockType.RockWall,
@@ -23,6 +30,25 @@ public class Caves : GenerationStep {
             if (y < 0 || x < 0 || elevations.Length <= x){
                 break;
             }
+            if (Random.value < keepDirectionWeight){
+                continue;
+            }
+            int rightIndex = directionIndex-1;
+            int leftIndex = directionIndex+1;
+            if (rightIndex < 0){
+                directionIndex = leftIndex;
+            } else if (Directions.Length <= leftIndex){
+                directionIndex = rightIndex;
+            } else {
+                float weightedMiddlePoint = 0.5f;
+                if (0 < Directions[leftIndex].y){
+                    weightedMiddlePoint = upwardsWeight;
+                } else if (0 < Directions[rightIndex].y){
+                    weightedMiddlePoint = 1-upwardsWeight;
+                }
+                directionIndex = Random.value < weightedMiddlePoint ? leftIndex : rightIndex;
+            }
+            direction = Directions[directionIndex];
         }
         return 1;
     }
