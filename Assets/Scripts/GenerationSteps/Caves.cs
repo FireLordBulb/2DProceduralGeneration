b253724d5de87cc66dc38d34513c238492331f73
FireLordBulb <79010333+FireLordBulb@users.noreply.github.com>
2024-08-23 14:24:33 +0200

    Added incomplete side walls at a constant width around the cave walk.

--- Assets/Scripts/GenerationSteps/Caves.cs
+++ Assets/Scripts/GenerationSteps/Caves.cs
@@ -15,24 +15,42 @@ public class Caves : GenerationStep {
     public override float Perform(BlockType[,] worldGrid, int[] elevations, WorldSize worldSize, Seed seed){
         int walkMaxSteps = (int)(worldSize.height*lengthScalar);
         Random.InitState((int)seed);
-        int x = Random.Range(0, elevations.Length);
-        int y = elevations[x];
+        Vector2Int position = new(Random.Range(0, elevations.Length), 0);
+        position.y = elevations[position.x];
         Vector2Int direction = Vector2Int.down;
         int directionIndex = Array.FindIndex(Directions, vector => vector == direction);
+
+        // TEMP
+        const int width = 6;
+        const int diagonalWidth = 4;
+        
+        /*Vector2Int previousLeftWallPosition = position+width*Vector2Int.right;
+        Vector2Int previousRightWallPosition = position-width*Vector2Int.right;*/
+        
         for (int i = 0; i < walkMaxSteps; i++){
-            worldGrid[x, y] = worldGrid[x, y] switch {
-                BlockType.Rock => BlockType.RockWall,
-                BlockType.Dirt or BlockType.Grass => BlockType.DirtWall,
-                _ => worldGrid[x, y]
-            };
-            x += direction.x;
-            y += direction.y;
-            if (y < 0 || x < 0 || elevations.Length <= x){
+
+            Vector2Int wallOffset = (direction.sqrMagnitude == 1 ? width : diagonalWidth)*Perpendicular(direction);
+            Vector2Int leftWallPosition = position+wallOffset;
+            Vector2Int rightWallPosition = position-wallOffset;
+            /*while ((previousLeftWallPosition-leftWallPosition).sqrMagnitude > 2){
+                
+            }
+            while ((previousRightWallPosition-rightWallPosition).sqrMagnitude > 2){
+                
+            }
+            previousLeftWallPosition = leftWallPosition;
+            previousRightWallPosition = rightWallPosition;
+            */
+            MakeCaveWall(worldGrid, leftWallPosition);
+            MakeCaveWall(worldGrid, rightWallPosition);
+            position += direction;
+            if (position.y < 0 || position.x < 0 || elevations.Length <= position.x){
                 break;
             }
             if (Random.value < keepDirectionWeight){
                 continue;
             }
+            
             int rightIndex = directionIndex-1;
             int leftIndex = directionIndex+1;
             if (rightIndex < 0){
@@ -51,5 +69,18 @@ public class Caves : GenerationStep {
             direction = Directions[directionIndex];
         }
         return 1;
+    }
+
+    private static void MakeCaveWall(BlockType[,] worldGrid, Vector2Int position){
+        worldGrid[position.x, position.y] = worldGrid[position.x, position.y] switch {
+            BlockType.Rock => BlockType.RockWall,
+            BlockType.Dirt or BlockType.Grass => BlockType.DirtWall,
+            _ => worldGrid[position.x, position.y]
+        };
+    }
+    
+    // Gives counter-clockwise perpendicular. // Why doesn't this already exist for Vector2Int, Unity!!!!
+    private static Vector2Int Perpendicular(Vector2Int vector){
+        return new Vector2Int(-vector.y, vector.x);
     }
 }
